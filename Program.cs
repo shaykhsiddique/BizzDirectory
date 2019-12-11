be1194ad62a9b64c2c707482ffb0f73e3c564035
Shaykh Siddique <shaykhsiddiqee@gmail.com>
2019-12-11 16:06:12 +0600

    fix name project bug

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace CourceMngr
+namespace BizzDirectory
 {
     public class Program
     {
